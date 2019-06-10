74902b7828630453f5376c96d572a88105252820
huhsame <seungyeon.huh@wrl.onl>
2019-06-10 13:47:31 +0900

    test material

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,12 @@
 // src/js/main.js
+import "@babel/polyfill";
 import { pi, power, Foo } from './lib';
+import {MDCRipple} from '@material/ripple';
+import {MDCTopAppBar} from '@material/top-app-bar';
+// Instantiation
+const topAppBarElement = document.querySelector('.mdc-top-app-bar');
+const topAppBar = new MDCTopAppBar(topAppBarElement);
+
 
 console.log(pi);
 console.log(power(pi, pi));
@@ -7,3 +14,5 @@ console.log(power(pi, pi));
 const f = new Foo();
 console.log(f.foo());
 console.log(f.bar());
+
+
