10e53004a1ca1608358e693c44f3c1e63b4041eb
huhsame <seungyeon.huh@wrl.onl>
2019-06-10 11:56:13 +0900

    setting webpack for bundling

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,9 @@
-const
+// src/js/main.js
+import { pi, power, Foo } from './lib';
+
+console.log(pi);
+console.log(power(pi, pi));
+
+const f = new Foo();
+console.log(f.foo());
+console.log(f.bar());
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     // enntry file
-    entry: './src/js/main.js',
+    entry: './src/js/index.js',
     // 컴파일 + 번들링된 js 파일이 저장될 경로와 이름 지정
     output: {
         path: path.resolve(__dirname, 'dist/js'),
