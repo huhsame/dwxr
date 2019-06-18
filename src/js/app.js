567f65f347ce4e71ae3bfecf5c7bc8bdaa92017e
huhsame <seungyeon.huh@wrl.onl>
2019-06-19 08:56:05 +0900

    Make the event of session login.

--- src/js/app.js
+++ src/js/app.js
@@ -17,8 +17,6 @@ import 'gun/lib/open';
 
 import 'bootstrap';
 
-// import 'aframe';
-
 
     ;(() => {
     function S(){};
@@ -26,32 +24,43 @@ import 'bootstrap';
     try{localStorage.clear(); // sessionStorage.clear();
     }catch(e){}
 
-    var opt = {};
+    let opt = {};
     opt.store = RindexedDB(opt);
     opt.localStorage = false; // Pass Gun({localStorage: false}) to disable localStorage.
-    opt.peers = ['https://d.wxr.onl/gun'];
-    // opt.peers = ['http://localhost:3000/gun'];
+    // opt.peers = ['https://d.wxr.onl/gun'];
+    opt.peers = ['http://localhost:3000/gun'];
     S.gun = Gun(opt);
 
     S.app = S.gun.get('root/test03');
     S.user = S.gun.user();
-    S.user.recall({sessionStorage: true});
+
+    let sslogin = new Event('sslogin');
+    S.user.recall({sessionStorage: true},()=>{
+        console.log(S.user.is.alias +' is here!');
+        document.dispatchEvent(sslogin);
+    });
 
     S.tell = (what, n) => {
-        let e = jq('#tell');
-        jq('#tell').find('.modal-body').text( what );
-        // console.log(jq('#tell').find('.modal-body'));
-        e.modal('show');
-        clearTimeout(S.tell.to);
-        S.tell.to = setTimeout(() => { e.modal('hide') }, n || 2500);
-        return what;
-    }
+    let e = jq('#tell');
+    jq('#tell').find('.modal-body').text( what );
+    // console.log(jq('#tell').find('.modal-body'));
+    e.modal('show');
+    clearTimeout(S.tell.to);
+    S.tell.to = setTimeout(() => { e.modal('hide') }, n || 2500);
+    return what;
+    };
 
     S.generateUUID = () => {
         return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
             let r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 3 | 8);
             return v.toString(16);
         });
+    };
+
+    S.getMyName = () => {
+        return S.user.is.alias;
     }
 })();
+
+
 
