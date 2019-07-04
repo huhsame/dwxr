195e9b53b1ed88e1e6bef63693e123cb9985e99a
huhsame <seungyeon.huh@wrl.onl>
2019-07-04 18:23:06 +0900

    Divide code as order of loading

---
+++ src/js/after-scene.js
@@ -0,0 +1,4 @@
+import "@babel/polyfill";
+
+import './nav-spave';
+import './init-space';
--- src/js/space.js
+++ src/js/space.js
@@ -1,17 +1,8 @@
-// src/js/main.js
 import "@babel/polyfill";
 
 import 'bootstrap';
 import 'aframe';
+
 import './three/TransformControls'
-import './aframe-components/transformControls';
-
-
-
-
-;(() => {
-
-})();
-
-
-
+import './aframe/components/transformControls';
+import './aframe/systems/postprocessing';
