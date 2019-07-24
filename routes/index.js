5680423a274a1ea41bce04800e0caac2ddea3578
huhsame <seungyeon.huh@wrl.onl>
2019-07-25 02:18:49 +0900

    Fix DEMO URL router

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 /* GET home page. */
 
 router.get('/web3d', function(req, res, next) {
-  res.redirect('/space/web3d');
+  res.redirect('/space/a13519ea-7f88-45e1-8668-8748ca93ae9c');
 });
 
 router.get('/', function(req, res, next) {
--- routes/spaces.js
+++ routes/spaces.js
@@ -3,12 +3,6 @@ var router = express.Router();
 
 /* GET spaces listing. */
 
-// a13519ea-7f88-45e1-8668-8748ca93ae9c
-
-router.get('/web3d', function(req, res, next) {
-  res.render('space', { spaceid: 'a13519ea-7f88-45e1-8668-8748ca93ae9c' });
-});
-
 router.get('/ar/:spaceid', function(req, res, next) {
   res.render('ar', { spaceid: req.params.spaceid });
 });
